34584dd154da5cd76173bb1c6ab089d299c6e73a
patelheta <patelheta385@gmail.com>
2022-12-01 23:29:20 -0800

    Add appointment components to Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 
+
 import DayList from "./DayList";
+import Appointment from "./Appointment/index.js";
 
 const days = [
   {
@@ -21,9 +23,47 @@ const days = [
     spots: 0,
   },
 ];
-
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +87,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) =>
+          <Appointment
+            key={appointment.id}
+            id={appointment.id}
+            time={appointment.time}
+            interview={appointment.interview}
+          />
+        )}
       </section>
     </main>
   );
