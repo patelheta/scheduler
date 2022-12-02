bb350d850b662cb816180a910741b5dae6d1eff6
patelheta <patelheta385@gmail.com>
2022-12-01 21:15:10 -0800

    Built first two child components Header and Empty for the Appointment

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+
 
 
 storiesOf("Button", module)
@@ -146,4 +149,7 @@ storiesOf("Appointment", module)
   .add("Appoinment", () => <Appointment />)
   .add("Appintment-time", () => <Appointment
     time="12pm"
+  />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}
   />);
