b5fcdb44741fdd72266f28304f346dd6e9722a76
patelheta <patelheta385@gmail.com>
2022-12-05 17:19:32 -0800

    Organizing our Logic

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,71 +1,20 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment/index.js";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from "../hooks/useApplicationData";
 
-import axios from "axios";
 
-export default function Application(props) {
 
-  function bookInterview(id, interview) {
-    return axios.put(`/api/appointments/${id}`, { interview }).then((response) => {
-      console.log(response);
-      if (response.status === 204) {
-        const appointment = {
-          ...state.appointments[id],
-          interview: { ...interview }
-        };
-        const appointments = {
-          ...state.appointments,
-          [id]: appointment
-        };
-        setState({
-          ...state,
-          appointments: appointments
-        });
-      }
-    });
-  }
-
-  function cancelInterview(id) {
-    return axios.delete(`/api/appointments/${id}`).then((response) => {
-
-      const appointment = {
-        ...state.appointments[id],
-        interview: null
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      setState({
-        ...state,
-        appointments: appointments
-      });
-    });
-  }
-
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, []);
+export default function Application() {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
-import classNames from "classnames";
 
 
 export default function interviewerList(props) {
