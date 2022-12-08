4b9581b213174d456df8289724662f2d665fe6a2
patelheta <patelheta385@gmail.com>
2022-12-07 21:21:00 -0800

    Writing Appointment Tests with cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -7,6 +7,6 @@ describe("Navigation", () => {
     cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.css", "day-list__item--selected", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected");
   });
 });
