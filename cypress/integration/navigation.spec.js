b8e12804f43f7ee86655dff8d1d19fd4a51d351f
patelheta <patelheta385@gmail.com>
2022-12-07 20:58:14 -0800

    Navigating Days

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,11 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.css", "day-list__item--selected", "rgb(242, 242, 242)");
+  });
 });
